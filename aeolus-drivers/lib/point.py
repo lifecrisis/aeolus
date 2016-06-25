6ddf7de12145d7c6516e1fd04189b8df8e1a7754
Jason Franklin <j_fra@fastmail.us>
2016-06-25 09:59:55 -0400

    Refactor on PMPoint __init__ method

--- aeolus-drivers/lib/point.py
+++ aeolus-drivers/lib/point.py
@@ -11,6 +11,6 @@ class PMPoint:
     def __init__(self, site_id, year, month, day, longitude, latitude, pm25):
         self.site_id = site_id
         self.date = datetime.datetime(int(year), int(month), int(day))
-        self.latitude = float(latitude)
         self.longitude = float(longitude)
+        self.latitude = float(latitude)
         self.pm25 = float(pm25)
