84f93f150f3e502c66347f9024bc494f079167ed
Jason Franklin <j_fra@fastmail.us>
2016-07-05 15:50:48 -0400

    Updated kfold.py library

--- aeolus-drivers/lib/kfold.py
+++ aeolus-drivers/lib/kfold.py
@@ -1,5 +1,5 @@
 """
-This module offers abstractions for performing k-fold cross validation.
+This module offers utilities for performing k-fold cross validation.
 
 Our learning method revolveds around k-fold cross validation (KFCV) with the
 following parameters:
@@ -15,14 +15,100 @@ depend on the particular random partition generated. Thus, in more advanced
 runs, we average error statistic results across partitions.
 """
 
+import copy
+import kdtree
+import math
+
 
 class KFoldConf:
-    pass
+
+    def __init__(self, folds, neighbors, power, radius, time_scale):
+        self.folds = folds
+        self.neighbors = neighbors
+        self.power = power
+        self.radius = radius
+        self.time_scale = time_scale
 
 
 def mare(conf, point_list_brd):
-    pass
+    """
+    Return the MARE error statistic generated from K-fold cross validation.
+
+    Take the given KFoldConf object and an ordered broadcasted list of point
+    objects and return the desired error statistic.
+    """
+
+    # deep copy point_list
+    points = copy.deepcopy(point_list_brd.value)
+
+    # scale time dimensions
+    for p in points:
+        p.scale_time(conf.time_scale)
+
+    # build a list of sets representing the relevant partition
+    partition = [list() for i in range(conf.folds)]
+    for i, p in enumerate(points):
+        partition[i % conf.folds].append(p)
+
+    results = [0.0 for i in range(conf.folds)]
+    for i in range(conf.folds):
+        # initialize validation set and training set
+        validation_set = partition[i]
+        training_set = list()
+        for j in range(conf.folds):
+            if j != i:
+                training_set += partition[j]
+        # build a kdtree from the training set
+        tree = kdtree.KDTree(training_set)
+        # compute result for this validation set
+        for p in validation_set:
+            nnl = tree.query(p, conf.neighbors)
+            results[i] += (abs(p.interpolate(nnl, conf.power) - p.value()) /
+                           p.value())
+        results[i] /= len(validation_set)
+
+    # return the average of the elements in the results vector
+    return sum(results) / len(results)
 
 
 def rmspe(conf, point_list_brd):
-    pass
+    """
+    Return the RMSPE error statistic generated from K-fold cross validation.
+
+    Take the given KFoldConf object and an ordered broadcasted list of point
+    objects and return the desired error statistic.
+    """
+
+    # deep copy point_list
+    points = copy.deepcopy(point_list_brd.value)
+
+    # scale time dimensions
+    for p in points:
+        p.scale_time(conf.time_scale)
+
+    # build a list of sets representing the relevant partition
+    partition = [list() for i in range(conf.folds)]
+    for i, p in enumerate(points):
+        partition[i % conf.folds].append(p)
+
+    results = [0.0 for i in range(conf.folds)]
+    for i in range(conf.folds):
+        # initialize validation set and training set
+        validation_set = partition[i]
+        training_set = list()
+        for j in range(conf.folds):
+            if j != i:
+                training_set += partition[j]
+        # build a kdtree from the training set
+        tree = kdtree.KDTree(training_set)
+        # compute result for this validation set
+        # TODO: Fix this result computation
+        for p in validation_set:
+            nnl = tree.query(p, conf.neighbors)
+            results[i] += ((p.interpolate(nnl, conf.power) - p.value()) /
+                           p.value()) ** 2.0
+        results[i] /= len(validation_set)
+        results[i] = math.sqrt(results[i]) * 100
+
+    # return the average of the elements in the results vector
+    return sum(results) / len(results)
