101813a87c2548a563e8991d9edf6b647e151f75
Jason Franklin <j_fra@fastmail.us>
2016-07-11 11:44:14 -0400

    Experiment 01 modified for cluster

--- aeolus-drivers/experiment_01/submit.sh
+++ aeolus-drivers/experiment_01/submit.sh
@@ -3,7 +3,7 @@
 spark-submit \
     --master yarn \
     --py-files kdtree.py,kfold.py,point.py \
-    --deploy-mode client \
+    --deploy-mode cluster\
     --name 'Experiment #01' \
     --num-executors 12 \
     --executor-cores 4 \
