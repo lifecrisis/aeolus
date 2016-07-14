3d02f4d57a9161ccf6a4db22df29a3d7464a58fe
Jason Franklin <j_fra@fastmail.us>
2016-07-14 13:08:53 -0400

    Implemented report() and load_partition() in 02

--- aeolus-drivers/experiment_02/experiment_02.py
+++ aeolus-drivers/experiment_02/experiment_02.py
@@ -12,20 +12,74 @@ Like the script from the previous experiment, this script produces a
 "results.csv" file in HDFS as output.
 """
 
-from pyspark import SparkConf, SparkContext
+import csv
+import StringIO
 
-CONF = SparkConf().setAppName('Experiment #02')
-SC = SparkContext(conf=CONF)
+import kfold
+import point
+
+# from pyspark import SparkConf, SparkContext
+#
+#
+# CONF = SparkConf().setAppName('Experiment #02')
+# SC = SparkContext(conf=CONF)
 
 
 def load_partition(partition_id):
-    # load partitions from HDFS, allows running in cluster mode...
-    pass
+    """
+    Return a list of PMPoints in the order of the given partition.
+
+    The kfold operations (mare, rmspe) are responsible for actually breaking
+    up a partition into a list of lists. The order of the loaded file
+    is what determins the "random" nature of this list of lists. Here, we
+    simply want the PMPoint objects in the proper order.
+    """
+    partition_filename = "partitions/partition_%02d.csv" % partition_id
+    f = open(partition_filename, 'r')
+
+    def load_record(record):
+        """ Parse a single CSV record. """
+        result = StringIO.StringIO(record)
+        fieldnames = ['site_id',
+                      'year',
+                      'month',
+                      'day',
+                      'longitude',
+                      'latitude',
+                      'pm25']
+        reader = csv.DictReader(result, fieldnames)
+        return reader.next()
+
+    result = map(lambda rec: point.PMPoint(**rec), map(load_record, f))
+    f.close()
+
+    return result
 
 
 def report(result_record):
-    # write results as CSV to HDFS, allows running in cluster mode...
-    pass
+    """
+    Return a report string (in CSV format) given a record from a result RDD.
+
+    This report method is unique to this experiment. This makes sense because
+    output differs across all experiments. Here, the "result_record" takes
+    the form:
+
+        (partition_id, KFoldConf, float, float)
+
+    The first element is an int. The last two are floating point numbers
+    storing the MARE and RMSPE results respectively.
+
+    This method also accesses a special "id" attribute added to KFoldConf
+    objects for grouping them later.
+    """
+    return (str(result_record[0]) +                     # partition_id
+            ',' + str(result_record[1].conf_id) +       # conf_id
+            ',' + str(result_record[1].folds) +         # folds
+            ',' + str(result_record[1].neighbors) +     # neighbors
+            ',' + str(result_record[1].power) +         # power
+            ',' + str(result_record[1].time_scale) +    # time_scale
+            ',' + str(result_record[2]) +               # MARE
+            ',' + str(result_record[3]) + '\n')         # RMSPE
 
 
 def main():
@@ -38,7 +92,7 @@ def main():
     #   (4) append results to RESULTLIST
     #   (5) i <- i + 1 and go to (2)
     #   Finally: Write RESULTLIST as CSV to HDFS
-    # TODO: Possibly add a conf ID in advance to group by them later
+    # TODO: add conf_id to KFoldConf objects in list
     pass
 
 if __name__ == "__main__":
